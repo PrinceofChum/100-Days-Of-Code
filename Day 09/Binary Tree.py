d362df1e5d72f01fff3807bfdd9c410fce4e77ed
ABISHEKRAJ R <56864775+PrinceofChum@users.noreply.github.com>
2021-01-02 21:09:45 +0530

    Create Binary Tree.py

---
+++ Day 09/Binary Tree.py
@@ -0,0 +1,46 @@
+class Node:
+
+    def __init__(self, data):
+
+        self.left = None
+        self.right = None
+        self.data = data
+
+# Insert method to create nodes
+    def insert(self, data):
+
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+# findval method to compare the value with nodes
+
+    def findval(self, lkpval):
+        if lkpval < self.data:
+            if self.left is None:
+                return str(lkpval)+" is not Found"
+            return self.left.findval(lkpval)
+        elif lkpval > self.data:
+            if self.right is None:
+                return str(lkpval)+" is not Found"
+            return self.right.findval(lkpval)
+        else:
+            return str(self.data) + " is found"
+# Print the tree
+
+    def PrintTree(self):
+        if self.left:
+            self.left.PrintTree()
+        print(self.data),
+        if self.right:
+            self.right.PrintTree()
+
