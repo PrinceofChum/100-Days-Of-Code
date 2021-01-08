8edac482f8569ebe43d2ac3ab78f6f8c97c9d984
ABISHEKRAJ R <56864775+PrinceofChum@users.noreply.github.com>
2021-01-08 21:30:58 +0530

    Create Compare two linked lists.py

---
+++ Day 07/HackerRank/Compare two linked lists.py
@@ -0,0 +1,8 @@
+def compare_lists(llist1, llist2):
+    while llist1 and llist2:
+        if llist1.data != llist2.data:
+            return 0
+        else:
+            llist1,llist2=llist1.next,llist2.next
+    return 1 if llist1 == llist2 else 0
+        
