f9bc16294d1002e9e79cccabaf01f4ff86058f86
ABISHEKRAJ R <56864775+PrinceofChum@users.noreply.github.com>
2021-01-11 23:19:12 +0530

    one of three modules

--- Day 18/Pickling/AdditionalReason.py
+++ Day 18/Pickling/AdditionalReason.py
@@ -1,5 +1,5 @@
 class AdditionalReason():
     def __init__(self,Reason):
-        self.Reason=Reason
+        self.Reason = Reason
     def getAdditionalReason(self):
         print("'N Of course ",self.Reason)
