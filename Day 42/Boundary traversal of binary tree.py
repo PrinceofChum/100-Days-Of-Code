6562f67b90b8885f907d00a90d0e380c388612e6
ABISHEKRAJ R <56864775+PrinceofChum@users.noreply.github.com>
2021-02-24 22:18:20 +0530

    Update Boundary traversal of binary tree.py

--- Day 42/Boundary traversal of binary tree.py
+++ Day 42/Boundary traversal of binary tree.py
@@ -2,7 +2,6 @@
   
 # A binary tree node 
 class Node: 
-  
     # Constructor to create a new node 
     def __init__(self, data): 
         self.data = data  
@@ -13,29 +12,23 @@ class Node:
 def printLeaves(root): 
     if(root): 
         printLeaves(root.left) 
-          
         # Print it if it is a leaf node 
         if root.left is None and root.right is None: 
             print(root.data), 
-  
         printLeaves(root.right) 
   
 # A function to print all left boundary nodes, except a  
 # leaf node. Print the nodes in TOP DOWN manner 
-def printBoundaryLeft(root): 
-      
+def printBoundaryLeft(root):       
     if(root): 
-        if (root.left): 
-              
+        if (root.left):               
             # to ensure top down order, print the node 
             # before calling itself for left subtree 
             print(root.data) 
             printBoundaryLeft(root.left) 
-          
         elif(root.right): 
             print (root.data) 
             printBoundaryLeft(root.right) 
-          
         # do nothing if it is a leaf node, this way we 
         # avoid duplicates in output 
   
@@ -43,38 +36,29 @@ def printBoundaryLeft(root):
 # A function to print all right boundary nodes, except 
 # a leaf node. Print the nodes in BOTTOM UP manner 
 def printBoundaryRight(root): 
-      
     if(root): 
         if (root.right): 
             # to ensure bottom up order, first call for 
             # right subtree, then print this node 
             printBoundaryRight(root.right) 
-            print(root.data) 
-          
+            print(root.data)           
         elif(root.left): 
             printBoundaryRight(root.left) 
-            print(root.data) 
-  
+            print(root.data)   
         # do nothing if it is a leaf node, this way we  
-        # avoid duplicates in output 
-  
-  
+        # avoid duplicates in output     
 # A function to do boundary traversal of a given binary tree 
 def printBoundary(root): 
     if (root): 
-        print(root.data) 
-          
+        print(root.data)           
         # Print the left boundary in top-down manner 
         printBoundaryLeft(root.left) 
-  
         # Print all leaf nodes 
         printLeaves(root.left) 
         printLeaves(root.right) 
-  
         # Print the right boundary in bottom-up manner 
-        printBoundaryRight(root.right) 
-  
-  
+        printBoundaryRight(root.right)   
+
 # Driver program to test above function 
 root = Node(20) 
 root.left = Node(8) 
