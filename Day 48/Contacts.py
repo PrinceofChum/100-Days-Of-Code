644f5d90d3e24e33dabfc3b95de5ee931e3c2976
ABISHEKRAJ R <56864775+PrinceofChum@users.noreply.github.com>
2021-03-02 23:01:06 +0530

    Update Contacts.py

--- Day 48/Contacts.py
+++ Day 48/Contacts.py
@@ -7,58 +7,39 @@ class Node:
             self.children = {}
         else:
             self.children = children
-
         self.is_leaf = is_leaf
-
         self.visited = visited
-
 
 def add(root, name):
     node = root
-
     node.visited += 1
-
     for key in node.children:
         pre, _key, _name = extract_prefix(key, name)
-
         if _key == '':
             # there is a match of a part of the key
             child = node.children[key]
             return add(child, _name)
-
         if pre != '':
             child = node.children[key]
-
             # need to split
             _node = Node(
                 children={_key: child},
                 visited=child.visited
             )
-
             del node.children[key]
             node.children[pre] = _node
-
             return add(_node, _name)
-
     node.children[name] = Node(is_leaf=True, visited=1)
-
 
 def find(root, name):
     node = root
-
     for key in node.children:
-
         pre, _key, _name = extract_prefix(key, name)
-
         if _name == '':
             return node.children[key].visited
-
         if _key == '':
             return find(node.children[key], _name)
-
     return 0
-
-
 
 def extract_prefix(str1, str2):
     n = 0
