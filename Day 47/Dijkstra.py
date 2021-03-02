b5d420675345fc0a9973925f11cdc41419e04b3a
ABISHEKRAJ R <56864775+PrinceofChum@users.noreply.github.com>
2021-03-02 06:30:27 +0530

    Update Dijkstra.py

--- Day 47/Dijkstra.py
+++ Day 47/Dijkstra.py
@@ -4,56 +4,43 @@
   
 # Library for INT_MAX 
 import sys 
-  
-class Graph(): 
-  
+  class Graph():   
     def __init__(self, vertices): 
         self.V = vertices 
         self.graph = [[0 for column in range(vertices)]  
                     for row in range(vertices)] 
-  
     def printSolution(self, dist): 
         print "Vertex \tDistance from Source"
         for node in range(self.V): 
-            print node, "\t", dist[node] 
-  
+            print node, "\t", dist[node]   
     # A utility function to find the vertex with  
     # minimum distance value, from the set of vertices  
     # not yet included in shortest path tree 
-    def minDistance(self, dist, sptSet): 
-  
+    def minDistance(self, dist, sptSet):   
         # Initilaize minimum distance for next node 
         min = sys.maxint 
-  
-        # Search not nearest vertex not in the  
-        # shortest path tree 
+        # Search not nearest vertex not in the shortest path tree 
         for v in range(self.V): 
             if dist[v] < min and sptSet[v] == False: 
                 min = dist[v] 
                 min_index = v 
-  
         return min_index 
   
     # Funtion that implements Dijkstra's single source  
     # shortest path algorithm for a graph represented  
     # using adjacency matrix representation 
     def dijkstra(self, src): 
-  
         dist = [sys.maxint] * self.V 
         dist[src] = 0
         sptSet = [False] * self.V 
-  
         for cout in range(self.V): 
-  
             # Pick the minimum distance vertex from  
             # the set of vertices not yet processed.  
             # u is always equal to src in first iteration 
             u = self.minDistance(dist, sptSet) 
-  
             # Put the minimum distance vertex in the  
             # shotest path tree 
             sptSet[u] = True
-  
             # Update dist value of the adjacent vertices  
             # of the picked vertex only if the current  
             # distance is greater than new distance and 
@@ -62,7 +49,6 @@ class Graph():
                 if self.graph[u][v] > 0 and sptSet[v] == False and \ 
                 dist[v] > dist[u] + self.graph[u][v]: 
                         dist[v] = dist[u] + self.graph[u][v] 
-  
         self.printSolution(dist) 
   
 # Driver program 
