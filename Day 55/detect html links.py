d9d0717a934a2e42e95f2ac61f0e1036b4f9dca9
ABISHEKRAJ R <56864775+PrinceofChum@users.noreply.github.com>
2021-03-11 22:50:38 +0530

    Update detect html links.py

--- Day 55/detect html links.py
+++ Day 55/detect html links.py
@@ -1,1 +1,10 @@
-pass
+import re
+
+pat = r'<a href="(.*?)".*?>([\w ,./]*)(?=</)'
+
+N = int(input())
+for _ in range(N):
+    html = input()
+    res = re.findall(pat, html)
+    for link, title in res:
+        print (link+","+title.strip())
